0ee1add83d3607094cee8aac4d953ad5d110adc4
Carlos Vinicius <viniciusdev.26@gmail.com>
2021-09-01 13:29:47 -0300

    pegar a localização do usuario e jogar no mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
 import { Map, TileLayer } from 'react-leaflet';
@@ -9,6 +9,18 @@ import local from '../images/local.svg';
 import '../styles/pages/OrphanagesMap.css';
 
 function OrphanagesMap() {
+  const [latitude, setLatitude] = useState(28.4811732);
+  const [longitude, setLongitude] = useState(-81.3426652);
+  
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const {latitude, longitude} = position.coords
+  
+      setLatitude(latitude);
+      setLongitude(longitude);
+    });
+  })
+
   return (
     <div id="page-map">
       <aside>
@@ -26,7 +38,7 @@ function OrphanagesMap() {
       </aside>
 
       <Map 
-        center={[-3.8265599,-38.4744777]}
+        center={[latitude, longitude]}
         zoom={15}
         style={{ width: '100%', height: '100%' }}
       >
